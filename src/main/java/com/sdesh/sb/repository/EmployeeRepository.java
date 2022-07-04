876986bec18c1e556c5c4370a74d6d11a2cc7a76
Sugumar Deshikan <sukumar.deshikan@gmail.com>
2022-07-04 08:07:19 +0530

    Base is ready!!

---
+++ src/main/java/com/sdesh/sb/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.sdesh.sb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sdesh.sb.model.Employee;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+}
